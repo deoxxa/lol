b8e139850635776029f7fa5b56f3fd57ba9d2a2c
deoxxa <deoxxa@fknsrs.biz>
2013-04-24 16:30:12 +1000

    convert exclamations object, add explanations

--- lol.js
+++ lol.js
@@ -1,11 +1,3 @@
-var exclamations = [
-    'lol',
-    'rofl',
-    'wtf',
-    'OMG',
-    'OMGWTFBBQ',
-];
-var length = exclamations.length;
 var LOL = function LOL(lol, wat) {
     this.lol = lol;
     this.wat = wat;
@@ -14,6 +6,31 @@ var LOL = function LOL(lol, wat) {
 LOL.prototype.toString = LOL.prototype.toJSON = function() {
     return this.lol;
 };
+
+var exclamations = {
+    'lol': [
+        '<l>augh <o>ut <l>oud',
+    ],
+    'rofl': [
+        '<r>olling <o>n the <f>loor <l>aughing',
+    ],
+    'wtf': [
+        '<w>hat <t>he <f>uck',
+        '<w>ell <t>hat\'s <f>unny',
+    ],
+    'omg': [
+        '<o>h <m>y <g>od',
+    ],
+    'omgwtfbbq': [
+        '<o>h <m>y <g>od <w>hat <t>he <f>uck <b><b><q>',
+    ],
+};
+
+var lols = Object.keys(exclamations).map(function(e) {
+    return new LOL(e, exclamations[e]);
+});
+
+var length = lols.length;
 
 module.exports = function lol() {
     var i = parseInt(Math.random()*length, 10);
