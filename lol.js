30c28aff17ab6ab3fd5083733473ab4b8352ba29
deoxxa <deoxxa@fknsrs.biz>
2013-04-24 16:29:02 +1000

    add LOL class

--- lol.js
+++ lol.js
@@ -6,6 +6,14 @@ var exclamations = [
     'OMGWTFBBQ',
 ];
 var length = exclamations.length;
+var LOL = function LOL(lol, wat) {
+    this.lol = lol;
+    this.wat = wat;
+};
+
+LOL.prototype.toString = LOL.prototype.toJSON = function() {
+    return this.lol;
+};
 
 module.exports = function lol() {
     var i = parseInt(Math.random()*length, 10);
